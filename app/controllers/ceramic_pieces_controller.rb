46f12f5def1a2e7bf455de9f325fd217d4d1db5a
Ronalde Monezzi <ronalde.monezzi@gmail.com>
2023-08-17 16:09:13 -0300

    calc

--- app/controllers/ceramic_pieces_controller.rb
+++ app/controllers/ceramic_pieces_controller.rb
@@ -1,4 +1,6 @@
 class CeramicPiecesController < ApplicationController
+  before_action :set_ceramic_piece, only: [:show]
+
   def new
     @ceramic_piece = CeramicPiece.new
   end
@@ -7,6 +9,7 @@ class CeramicPiecesController < ApplicationController
     @ceramic_piece = CeramicPiece.new(ceramic_piece_params)
 
     if @ceramic_piece.save
+      # Once saved, we can get all the calculations because it's saved in the model.
       redirect_to @ceramic_piece, notice: 'Cost was successfully calculated.'
     else
       render :new
@@ -14,10 +17,14 @@ class CeramicPiecesController < ApplicationController
   end
 
   def show
-    @ceramic_piece = CeramicPiece.find(params[:id])
+    # The `set_ceramic_piece` method already finds the ceramic_piece for us.
   end
 
   private
+
+  def set_ceramic_piece
+    @ceramic_piece = CeramicPiece.find(params[:id])
+  end
 
   def ceramic_piece_params
     params.require(:ceramic_piece).permit(:height, :width, :thickness, :clay_cost, :nail_polish_cost, :electricity_cost, :burn_duration, :oven_power, :maintenance_cost, :labor_cost)
--- app/models/ceramic_piece.rb
+++ app/models/ceramic_piece.rb
@@ -11,12 +11,33 @@ class CeramicPiece < ApplicationRecord
   validates :maintenance_cost, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :labor_cost, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
-  # Method to compute the total cost of the ceramic piece
+  def clay_cost_calculation
+    volume = height * width * thickness
+    volume * clay_cost
+  end
+
+  def nail_polish_cost_calculation
+    surface_area = 2 * (height * width + height * thickness + width * thickness)
+    surface_area * nail_polish_cost
+  end
+
+  def electricity_cost_calculation
+    electricity_cost * burn_duration * oven_power
+  end
+
+  def maintenance_cost_calculation
+    maintenance_cost * burn_duration # Assuming maintenance cost increases with burn duration
+  end
+
+  def labor_cost_calculation
+    labor_cost * burn_duration # Assuming labor cost is proportional to burn duration
+  end
+
   def total_cost
-    clay_cost +
-    nail_polish_cost +
-    (electricity_cost * burn_duration * oven_power) +
-    maintenance_cost +
-    labor_cost
+    clay_cost_calculation +
+    nail_polish_cost_calculation +
+    electricity_cost_calculation +
+    maintenance_cost_calculation +
+    labor_cost_calculation
   end
 end
